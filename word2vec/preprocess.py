9eaed771ab2c40ab968046b067cd97a7d92981a5
aprilis <jkwiecien97@gmail.com>
2017-01-13 21:40:29 +0100

    poprawiony preprocessing, dodane 2k ksiazek

--- word2vec/preprocess.py
+++ word2vec/preprocess.py
@@ -1,12 +1,36 @@
-import nltk
 import sys
+
+def preprocess(line):
+    line = line.decode('utf8').lower()
+    tokens = []
+    last = u''
+    for c in line:
+        if c in u"'-" or c.isalpha():
+            last += c
+        else:
+            if last != u'': tokens.append(last)
+            last = u''
+            if not c.isspace():
+                tokens.append(c)
+        if len(last) >= 2 and last[-2:] == u"''":
+            last = last.rstrip("'")
+            if last != u'': tokens.append(last)
+            tokens.append(u"''")
+            last = u''
+            
+    if last != u'': tokens.append(last)
+    return u' '.join(tokens).encode('utf8')
 
 if len(sys.argv) < 2:
     print 'Usage: python preprocess.py <in> <out>'
+    exit(0)
 
-with open(sys.argv[1], 'r') as file:
-    text = file.read().decode('utf8').lower()
-tokens = nltk.word_tokenize(text)
-text2 = u' '.join(tokens)
-with open(sys.argv[2], 'w') as file:
-    file.write(text2.encode('utf8'))
+if sys.argv[1] == sys.argv[2]:
+    print 'in and out should be different paths'
+    exit(0)
+
+with open(sys.argv[1], 'r') as f_in, open(sys.argv[2], 'w') as f_out:
+    while True:
+        line = f_in.readline()
+        if line == '': break
+        f_out.write(preprocess(line) + '\n')
--- word2vec/train.sh
+++ word2vec/train.sh
@@ -4,9 +4,9 @@ cat data/*.txt > data/_train1.tmp
 echo "python preprocess.py data/_train1.tmp data/_train2.tmp"
 echo "this may take a loooong time..."
 python preprocess.py data/_train1.tmp data/_train2.tmp
-echo "bin/word2phrase -train data/_train2.tmp -output data/_train3.tmp -threshold 200 -debug 2"
-bin/word2phrase -train data/_train2.tmp -output data/_train3.tmp -threshold 200 -debug 2
-echo "bin/word2vec -train data/_train3.tmp -output data/vec.bin -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -cbow 1 -iter 3 -binary 1"
-bin/word2vec -train data/_train3.tmp -output data/vec.bin -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -cbow 1 -iter 3 -binary 1
+echo "bin/word2phrase -train data/_train2.tmp -output data/_train3.tmp -threshold 500 -debug 2"
+bin/word2phrase -train data/_train2.tmp -output data/_train3.tmp -threshold 500 -debug 2
+echo "bin/word2vec -train data/_train3.tmp -output data/vec.bin -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -cbow 1 -iter 3 -binary 1 -threads 3"
+bin/word2vec -train data/_train3.tmp -output data/vec.bin -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -cbow 1 -iter 3 -binary 1 -threads 3
 echo "cleanup"
 rm data/_train*.tmp
