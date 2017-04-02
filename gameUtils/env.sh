2b96430cb5649dfe624f0f85b34feaa725ee12f8
Bartosz Kostka <kostka@weaselcrow.com>
2017-04-02 23:22:49 +0200

    ztools

--- gameUtils/env.sh
+++ gameUtils/env.sh
@@ -1,3 +1,7 @@
+wget http://mirror.ifarchive.org/if-archive/infocom/tools/ztools/ztools731.tar.gz
+mkdir ztools731/
+tar -xvzf ztools731.tar.gz -C ztools731/
+make -C ztools731/ all
 wget http://edmonson.paunix.org/rezrov/Games-Rezrov-0.20.zip
 unzip Games-Rezrov-0.20.zip
 cd Games-Rezrov-0.20/
@@ -8,3 +12,4 @@ sudo make install
 cd ..
 chmod +x *.sh
 ./all.sh Games-Rezrov-0.20/minizork.z3 minizork-example/
+./ztools731/txd Games-Rezrov-0.20/minizork.z3 >minizork-example/fullgame.txt
