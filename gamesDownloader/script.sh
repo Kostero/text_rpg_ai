e1c2afd46035fb57f8be0b8a959d918b2cd19a41
Bartosz Kostka <kostka@weaselcrow.com>
2017-04-16 23:25:17 +0200

    fixed proper removing of directories

--- gamesDownloader/script.sh
+++ gamesDownloader/script.sh
@@ -3,5 +3,4 @@ for i in `ls *.zip`; do yes | unzip $i; done
 mkdir games
 mv */*.{z,Z}{1,2,3,4,5,6,7,8,9,blorb} games/
 mv *.{z,Z}{1,2,3,4,5,6,7,8,9,blorb} games/
-find . -maxdepth 1 -type f ! -name 'script.sh' -delete
-find . -type d ! -name 'games' -delete
+rm -r !(games|script.sh)
