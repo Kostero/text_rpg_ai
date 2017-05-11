aa822892a63cf0e02f82691fa8a45ae3d4306667
Bartosz Kostka <kostka@weaselcrow.com>
2017-05-11 11:41:57 +0200

    env.sh fix

--- env.sh
+++ env.sh
@@ -1,10 +1,11 @@
-git clone https://github.com/danielricks/textplayer.git
+#git clone https://github.com/danielricks/textplayer.git
 cd textplayer
 git clone https://github.com/DavidGriffith/frotz.git
 cd frotz
 make dumb
 cd ../..
-pip install -r requirements.txt
+pip install cython --user
+pip install -r requirements.txt --user
 # install nltk data
 python -m nltk.downloader all
 git clone https://github.com/emilmont/pyStatParser.git
