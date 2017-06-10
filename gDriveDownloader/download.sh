e6c3cf7c9c26c567807f7c9170bb2f63fb2c0d3f
Bartosz Kostka <kostka.bartosz@gmail.com>
2017-06-10 21:00:57 +0200

    and rename large files

--- gDriveDownloader/download.sh
+++ gDriveDownloader/download.sh
@@ -1,9 +1,9 @@
 ./gDriveDownloader.sh 0B3lpCS07rg43bVBmd1lSVUVSb28 scholar_required_files.zip
 ./gDriveDownloader.sh 0B7XkCwpI5KDYNlNUTTlSS21pQmM GoogleNews-vectors-negative300.bin.gz
 gunzip GoogleNews-vectors-negative300.bin.gz
-mv GoogleNews-vectors-negative300.bin.gz ../word2vec/
+mv GoogleNews-vectors-negative300.bin ../word2vec/data/googlenews.bin
 unzip scholar_required_files.zip
-mv postagged_wikipedia_for_word2vec.bin ../word2vec/
+mv postagged_wikipedia_for_word2vec.bin ../word2vec/wikipedia.bin
 rm post* scholar_required_files.zip 
 
 
