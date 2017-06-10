56b9588e61de35b40c4ada36bb2b5ed5e06086e1
Bartosz Kostka <kostka.bartosz@gmail.com>
2017-06-10 20:59:33 +0200

    download large files

--- gDriveDownloader/download.sh
+++ gDriveDownloader/download.sh
@@ -1,2 +1,9 @@
 ./gDriveDownloader.sh 0B3lpCS07rg43bVBmd1lSVUVSb28 scholar_required_files.zip
 ./gDriveDownloader.sh 0B7XkCwpI5KDYNlNUTTlSS21pQmM GoogleNews-vectors-negative300.bin.gz
+gunzip GoogleNews-vectors-negative300.bin.gz
+mv GoogleNews-vectors-negative300.bin.gz ../word2vec/
+unzip scholar_required_files.zip
+mv postagged_wikipedia_for_word2vec.bin ../word2vec/
+rm post* scholar_required_files.zip 
+
+
