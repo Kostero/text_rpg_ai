14b11fc1ff9bbcbc2c1f3a88106a74a29e02850c
Bartosz Kostka <kostka.bartosz@gmail.com>
2017-06-10 20:51:02 +0200

    gDriveDownloader

---
+++ gDriveDownloader/download.sh
@@ -0,0 +1,2 @@
+./gDriveDownloader.sh 0B3lpCS07rg43bVBmd1lSVUVSb28 scholar_required_files.zip
+./gDriveDownloader.sh 0B7XkCwpI5KDYNlNUTTlSS21pQmM GoogleNews-vectors-negative300.bin.gz
---
+++ gDriveDownloader/gDriveDownloader.sh
@@ -0,0 +1,2 @@
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=$1" > /tmp/intermezzo.html
+curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > $2
